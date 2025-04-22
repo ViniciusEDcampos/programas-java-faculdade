ce7f930eff0b0d4d1ab1a35e595ee0e2b975ed9d
Vinicius Eduardo <84870190+ViniciusEDcampos@users.noreply.github.com>
2025-04-22 15:39:04 -0300

    Tabela de Multiplicação do Número 5
    Programa que imprime uma tabela de multiplicação do número 5, de 1 até 10, utilizando um loop for .

---
+++ loop/for/LoopForTabelaNumero5.java
@@ -0,0 +1,20 @@
+
+package loopfortabelanumero5;
+
+/**
+ *
+ * @author ACER
+ */
+public class LoopForTabelaNumero5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        for(int i=0;i<11;i++){
+            System.out.println("5 X " + i + " = " + 5 * i);
+        }
+    }
+}
