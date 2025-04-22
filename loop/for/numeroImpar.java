84af45e522de674c5fd5f4a58f0dac8e25f9ad4f
Vinicius Eduardo <84870190+ViniciusEDcampos@users.noreply.github.com>
2025-04-22 16:05:35 -0300

    Impressão de Números Ímpares
    Programa que utiliza um loop para imprimir todos os números ímpares entre 1 e 100.

---
+++ loop/for/numeroImpar.java
@@ -0,0 +1,25 @@
+
+package javaapplication19;
+
+/**
+ *
+ * @author ACER
+ */
+public class numeroImpar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        for(int i =0; i<100;i++){
+            
+            i = i + 1;
+            System.out.println(i);   
+        }
+
+
+    }
+    
+}
