505be417e5c5837eb19111652b82b734dae7300f
Vinicius Eduardo <84870190+ViniciusEDcampos@users.noreply.github.com>
2025-04-22 14:32:27 -0300

    loop while para imprimir do numero 1 ao 50
    loop utilizando while para imprimir do numero 1 ao 50 em java.

---
+++ loop/loopWhile/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int numeros = 0;
+        
+        while(numeros<50){
+            numeros++;
+            System.out.println(numeros);
+        }
+    }
+}
